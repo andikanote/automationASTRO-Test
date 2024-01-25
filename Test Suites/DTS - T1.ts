e1afe9894441228a57b3d056cdb088bb42069841
Andika Prananto <andika.prananto@dagangan.com>
2024-01-25 22:46:42 +0700

    Integrated TestRail

---
+++ Test Suites/DTS - T1.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<FilteringTestSuiteEntity>
+   <description></description>
+   <name>DTS - T1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>110c6c92-9148-492c-afdf-fcb49d3307a8</testSuiteGuid>
+   <filteringBuiltIn></filteringBuiltIn>
+   <filteringExtension>com.katalon.plugin.dynamic_execution.TestRailFilteringTestSuiteImpl</filteringExtension>
+   <filteringPlugin>com.katalon.katalon-studio-testrail-plugin</filteringPlugin>
+   <filteringText>R2</filteringText>
+</FilteringTestSuiteEntity>
---
+++ Test Suites/TS - S1.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS - S1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9b260cb8-315e-469a-b1c7-c76ad62d3be8</testSuiteGuid>
+   <testCaseLink>
+      <guid>0b140a53-ec60-4851-93e6-289feacdfd5a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/OPEN APPLICATION</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e8bf013c-3af5-4b3f-a1ff-1b95d3c27f26</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ON BOARDING APP</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>768ca553-2bcb-4901-9bdd-eaac9b82117a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC-LOGIN</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
